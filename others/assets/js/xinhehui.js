3b40843c6f95c19138a8d3a153d7226a20db6745
shixiaomu58 <1468443120@qq.com>
2016-12-12 15:43:59 +0800

    15.43

--- others/assets/js/xinhehui.js
+++ others/assets/js/xinhehui.js
@@ -1,17 +1,19 @@
 $(function(){
     $(".news-user-frame li").mouseover(function(){
-        if (!($(this).is(":animated")&&$(this).hasclass("isLoaded"))){
-            $(this).siblings("li").animated({"width":"150px"},"fast");//将其余同胞li元素设置动画：宽度快速变为150px.
+        if (!($(this).is(":animated")&&$(this).hasClass("isLoaded"))){
+            $(this).addClass("isLoaded");
+            $(this).siblings("li").removeClass("isLoaded");
+            $(this).siblings("li").animate({"width":"150px"},"fast");//将其余同胞li元素设置动画：宽度快速变为150px.
             $(this).siblings("li").find("h4").hide();//将其余同胞li元素下面的h4设置为隐藏
-            $(this).animated({"width":"422px"},"fast");//将当前li元素设置动画：宽度快速变为422px.
+            $(this).animate({"width":"422px"},"fast");//将当前li元素设置动画：宽度快速变为422px.
             $(this).find("h4").show();//将当前li元素下面的h4设置为隐藏
 
-            $(this).find(".user-play-bg-name").animated({"opacity": "0"});//当前左下角文字设为透明即隐藏
+            $(this).find(".user-play-bg-name").animate({opacity:0});//当前左下角文字设为透明即隐藏
             $(this).find(".button-user-play").show();//当前点击查看按钮设为：可见
             $(this).find(".iocn-user-em icon-user-zhiyin").hide();//当前左上角图片设为：隐藏
             $(this).find(".user-play-text").show();//当前左上角文字设为：可见
 
-            $(this).siblings("li").find(".user-play-bg-name").animated({"opacity": "1"});//当前左下角文字设为透明即隐藏
+            $(this).siblings("li").find(".user-play-bg-name").animate({opacity: 1});//当前左下角文字设为透明即隐藏
             $(this).siblings("li").find(".button-user-play").hide();//当前点击查看按钮设为：可见
             $(this).siblings("li").find(".iocn-user-em icon-user-zhiyin").show();//当前左上角图片设为：隐藏
             $(this).siblings("li").find(".user-play-text").hide();//当前左上角文字设为：可见
