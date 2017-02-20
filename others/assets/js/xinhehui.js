5df0de51774c8bea2eaa13084e6d775e2eaf4828
shixiaomu58 <1468443120@qq.com>
2017-02-20 19:55:35 +0800

    2月20号

--- others/assets/js/xinhehui.js
+++ others/assets/js/xinhehui.js
@@ -1,6 +1,6 @@
 $(function(){
     $(".news-user-frame li").mouseover(function(){
-        if (!($(this).is(":animated")&&$(this).hasClass("isLoaded"))){
+        if (!($(this).is(":animated")||$(this).hasClass("isLoaded"))){
             $(this).addClass("isLoaded");
             $(this).siblings("li").removeClass("isLoaded");
             $(this).siblings("li").animate({"width":"150px"},"fast");//将其余同胞li元素设置动画：宽度快速变为150px.
@@ -8,14 +8,14 @@ $(function(){
             $(this).animate({"width":"422px"},"fast");//将当前li元素设置动画：宽度快速变为422px.
             $(this).find("h4").show();//将当前li元素下面的h4设置为隐藏
 
-            $(this).find(".user-play-bg-name").animate({opacity:0});//当前左下角文字设为透明即隐藏
+            $(this).find(".user-play-bg-name").animate({opacity:"0"});//当前左下角文字设为透明即隐藏
             $(this).find(".button-user-play").show();//当前点击查看按钮设为：可见
-            $(this).find(".iocn-user-em icon-user-zhiyin").hide();//当前左上角图片设为：隐藏
+            $(this).find(".iocn-user-em").hide();//当前左上角图片设为：隐藏
             $(this).find(".user-play-text").show();//当前左上角文字设为：可见
 
-            $(this).siblings("li").find(".user-play-bg-name").animate({opacity: 1});//当前左下角文字设为透明即隐藏
+            $(this).siblings("li").find(".user-play-bg-name").animate({opacity: "1"});//其他左下角身为显示
             $(this).siblings("li").find(".button-user-play").hide();//当前点击查看按钮设为：可见
-            $(this).siblings("li").find(".iocn-user-em icon-user-zhiyin").show();//当前左上角图片设为：隐藏
+            $(this).siblings("li").find(".iocn-user-em").show();//当前左上角图片设为：隐藏
             $(this).siblings("li").find(".user-play-text").hide();//当前左上角文字设为：可见
         }
       })
@@ -31,7 +31,7 @@ $(function(){
              $(".news-user-frame li").find(".button-user-play").hide();//点击查看按钮
              $(".news-user-frame li").animate({width:"241px"},300);//宽度设为241px
              $(".news-user-frame li").find("h4").show();//h4
-             $(".news-user-frame li").find(".user-play-bg-name").animate({opacity:1});//左下文字
+             $(".news-user-frame li").find(".user-play-bg-name").animate({opacity:"1"});//左下文字
              $(".news-user-frame li").find(".iocn-user-em").show();//左上图片
         }
      })
